ac456a9099d9a6975a62abbb75fada792fe93266
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-18 12:01:35 -0500

    Add getJSON, add load stock

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,42 @@
-var stockApp = angular.module('stockApp', []);
+var stocksApp = angular.module('stocksApp', []);
 
-stockApp.controller('stocksController', function ($scope, $http){
+stocksApp.controller('stocksController', function ($scope, $http){
+
+	$scope.getStocks = function(){
+		var encodedTickers = encodeURIComponent($scope.userStocks);
+		url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+
+		$http.get(url).success(function (stockData){
+			// console.log(stockData);
+			// console.log(encodedTickers);
+			$scope.listOfStocks = stockData.query.results.quote;
+
+			// $scope.loadStock($scope)
+
+// console.log(stockData.query.results);
+// console.log([stockData.query.results]);
+
+		})
+
+	}
+
+	$scope.loadStock = function(stockData){
+		// init array
+		$scope.dataList = [];
+		console.log(stockData);
+		for(name in stockData){
+			// console.log(name);
+			$scope.dataList.push({
+				prop: name,
+				value: stockData[name]
+			})
+		}
+	}
+
 
 });
+
+
+
+
+
