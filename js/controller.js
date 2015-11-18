21e3c0a694ff0703ee11d0c67037a01806f558b5
Robert Bunch <robert.bunch@tinroofsoftware.com>
2015-11-18 16:12:52 -0500

    Add dummy json to make Yahoo happy, add class

--- js/controller.js
+++ js/controller.js
@@ -3,24 +3,46 @@ var stocksApp = angular.module('stocksApp', []);
 stocksApp.controller('stocksController', function ($scope, $http){
 
 	$scope.getStocks = function(){
+
 		var encodedTickers = encodeURIComponent($scope.userStocks);
 		url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
 
-		$http.get(url).success(function (stockData){
+		// $http.get(url).success(function (stockData){
 			// console.log(stockData);
 			// console.log(encodedTickers);
-			$scope.listOfStocks = stockData.query.results.quote;
+			// $scope.listOfStocks = stockData.query.results.quote;
+
+
+			$scope.listOfStocks2 = [
+			{
+				Symbol: "Goog",
+				Change: "+1.2",
+				Name: "Google"
+			},
+			{
+				Symbol: "Yhoo",
+				Change: "-1.2",
+				Name: "Yahoo"
+			},
+			{
+				Symbol: "X",
+				Change: "+2.2",
+				Name: "Trinity Trains"
+			}
+			];
+
 
 			// $scope.loadStock($scope)
 
 // console.log(stockData.query.results);
 // console.log([stockData.query.results]);
 
-		})
+		// })
 
 	}
 
 	$scope.loadStock = function(stockData){
+
 		// init array
 		$scope.dataList = [];
 		console.log(stockData);
@@ -33,6 +55,13 @@ stocksApp.controller('stocksController', function ($scope, $http){
 		}
 	}
 
+	$scope.getChangeClass = function (change){
+		if(change.indexOf('+') > -1){
+			return 'change-positive';
+		}else if (change.indexOf('-') > -1) {
+			return 'change-negative';
+		}
+	}
 
 });
 
